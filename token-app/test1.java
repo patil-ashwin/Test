ff375837c868514cb57548fbd394751918610514
Ashwin Patil <p.ashwin@hotmail.com>
2024-12-04 12:29:02 +0530

    Update test1.java

--- token-app/test1.java
+++ token-app/test1.java
@@ -1,29 +1,33 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
 import java.util.Map;
+import java.util.Properties;
 
-@Component
-public class EnvironmentLogger {
+@Configuration
+public class EnvironmentLoggerConfig {
 
-    private static final Logger log = LoggerFactory.getLogger(EnvironmentLogger.class);
+    private static final Logger log = LoggerFactory.getLogger(EnvironmentLoggerConfig.class);
 
-    private final Environment environment;
+    @Bean
+    public CommandLineRunner logEnvironmentVariables(Environment environment) {
+        return args -> {
+            log.info("### Logging Environment Variables ###");
+            Map<String, String> envVars = System.getenv();
+            envVars.forEach((key, value) -> log.info("{}={}", key, value));
 
-    public EnvironmentLogger(Environment environment) {
-        this.environment = environment;
-    }
+            log.info("### Logging System Properties ###");
+            Properties systemProperties = System.getProperties();
+            systemProperties.forEach((key, value) -> log.info("{}={}", key, value));
 
-    @PostConstruct
-    public void logEnvironment() {
-        log.info("### Logging Environment Variables ###");
-        Map<String, String> envVars = System.getenv();
-        envVars.forEach((key, value) -> log.info("{}={}", key, value));
-
-        log.info("### Logging Spring Environment Properties ###");
-        environment.getSystemProperties().forEach((key, value) -> log.info("{}={}", key, value));
+            log.info("### Logging Spring Environment Properties ###");
+            for (String propertyName : environment.getPropertySources().toString().split("\n")) {
+                log.info(propertyName.trim());
+            }
+        };
     }
 }
