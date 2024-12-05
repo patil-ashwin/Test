930f25cbdef0290c82fef6a8981aca38c503a05c
Ashwin Patil <p.ashwin@hotmail.com>
2024-12-06 00:05:23 +0530

    Update test1.java

--- token-app/test1.java
+++ token-app/test1.java
@@ -1,30 +1,10 @@
-server:
-  ssl:
-    key-store: classpath:your-keystore.jks
-    key-store-password: your-password
-    key-password: your-key-password
-
-feign:
-  httpclient:
-    enabled: true
-
-custom:
-  ssl:
-    trust-store: classpath:your-keystore.jks
-    trust-store-password: your-password
-
-
-    import feign.Client;
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.ssl.SSLContextBuilder;
+import feign.Client;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.net.ssl.SSLContext;
-import java.io.File;
+import javax.net.ssl.TrustManagerFactory;
+import java.io.InputStream;
 import java.security.KeyStore;
 
 @Configuration
@@ -32,19 +12,21 @@ public class FeignClientConfig {
 
     @Bean
     public Client feignClient() throws Exception {
-        // Load the JKS
+        // Load your JKS file
         KeyStore keyStore = KeyStore.getInstance("JKS");
-        keyStore.load(getClass().getResourceAsStream("/your-keystore.jks"),
-                      "your-password".toCharArray());
+        try (InputStream keyStoreStream = getClass().getResourceAsStream("/your-keystore.jks")) {
+            keyStore.load(keyStoreStream, "your-password".toCharArray());
+        }
 
-        // Configure SSLContext
-        SSLContext sslContext = SSLContextBuilder.create()
-                .loadKeyMaterial(keyStore, "your-key-password".toCharArray())
-                .build();
+        // Initialize TrustManager with the JKS
+        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+        trustManagerFactory.init(keyStore);
 
-        SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE);
-        CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(socketFactory).build();
+        // Create SSLContext
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, trustManagerFactory.getTrustManagers(), null);
 
-        return new Client.Default(httpClient, null);
+        // Use Feign Client with the SSL Socket Factory
+        return new Client.Default(sslContext.getSocketFactory(), null);
     }
 }
